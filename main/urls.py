c8721ff1a99045864147d20ed8306db670a0bcd6
Asakil <asakilsarhan@gmail.com>
2023-11-07 10:47:05 +0800

    update

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path , include
-
-app_name = 'main'
-
-urlpatterns = [
-    path('', views.index , name = 'index'),
-    path('broadcast.html', views.broadcast, name='broadcast'),
-]
